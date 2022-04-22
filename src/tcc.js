0ddce9ff2a2b1dbeac792a1aad5152f03ce9b0eb
Ned-Tom <tdj800@gmail.com>
2022-04-22 23:29:37 +0200

    Changed logboek info message

--- src/tcc.js
+++ src/tcc.js
@@ -5,4 +5,4 @@ customElements.define('tcc-room', tccroomcard)
 customElements.define('tcc-bubble', tccbubblecard)
 
 // Message when loaded in log
-console.info(`%c✨ TCC ✨`, "color: white; font-weight: 700;")
+console.info(`%c✨ Tom's Custom Cards ✨ \n%cDev version Instaled!`, "color: white; font-weight: 700;" ,"color: gray; font-weight: 100;")
