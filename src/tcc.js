1757d39d42b6ae2c30dffb4ee32fc4fd7749c5ae
Ned-Tom <tdj800@gmail.com>
2022-04-22 22:43:08 +0200

    Moved card

---
+++ src/tcc.js
@@ -0,0 +1,8 @@
+// Main init location
+
+// Cards
+customElements.define('tcc-room', tccroomcard)
+customElements.define('tcc-bubble', tccbubblecard)
+
+// Message when loaded in log
+console.info(`%c✨ TCC ✨`, "color: white; font-weight: 700;")
