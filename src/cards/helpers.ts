164d45891fabdaf37659e6320608fbc4dc0a1266
Ned-Tom <tdj800@gmail.com>
2022-04-29 11:17:14 +0200

    Added helpers

---
+++ src/cards/helpers.ts
@@ -0,0 +1,14 @@
+interface RegisterCardParams {
+  type: string;
+  name: string;
+  description: string;
+}
+export function registerCustomCard(params: RegisterCardParams) {
+  const windowWithCards = window as unknown as Window & {
+      customCards: unknown[];
+  };
+  windowWithCards.customCards = windowWithCards.customCards || [];
+  windowWithCards.customCards.push({
+      preview: true,
+  });
+}
