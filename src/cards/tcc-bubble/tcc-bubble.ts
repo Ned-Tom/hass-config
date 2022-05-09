34e415eb6d48a388226694273b8d9b63cc628fc3
Ned-Tom <tdj800@gmail.com>
2022-05-09 10:44:41 +0200

    Improved shadow of Bubble card

--- src/cards/tcc-bubble/tcc-bubble.ts
+++ src/cards/tcc-bubble/tcc-bubble.ts
@@ -188,7 +188,7 @@ export class TCCBubbleCard extends LitElement {
       .tccBubble > .button{
         padding:8px;
         border-radius: 50%;
-        box-shadow: var(--ha-card-box-shadow);
+        box-shadow: var( --ha-card-box-shadow, 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12) );
         width: 60px;
         height: 60px;
         padding: 0;
