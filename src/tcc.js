452533d3ab0c5455669ab4d1ec5f2384a1e5739b
Ned-Tom <tdj800@gmail.com>
2022-05-09 10:29:49 +0200

    Removed compiled files

--- src/tcc.js
+++
@@ -1,8 +0,0 @@
-// Main init location
-
-// Cards
-customElements.define('tcc-room', tccroomcard)
-customElements.define('tcc-bubble', tccbubblecard)
-
-// Message when loaded in log
-console.info(`%c✨ Tom's Custom Cards ✨ \n%cDev version Instaled!`, "color: white; font-weight: 700;" ,"color: gray; font-weight: 100;")
