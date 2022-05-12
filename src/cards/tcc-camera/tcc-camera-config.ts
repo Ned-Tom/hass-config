16fcdbc94fa2b8512d90e59398f6c56ed6376787
Ned-Tom <tdj800@gmail.com>
2022-05-12 09:41:29 +0200

    Added base implementation of Camera Card

---
+++ src/cards/tcc-camera/tcc-camera-config.ts
@@ -0,0 +1,27 @@
+import { ActionConfig, LovelaceCard, LovelaceCardConfig, LovelaceCardEditor } from 'custom-card-helpers';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'boilerplate-card-editor': LovelaceCardEditor;
+    'hui-error-card': LovelaceCard;
+  }
+}
+
+// TODO Add your configuration elements here for type-checking
+export interface TCCCameraCardConfig extends LovelaceCardConfig {
+  entity?: string;
+  // type: string;
+  name?: string;
+  // room_name?: string;
+  // room_icon?: string;
+  // room_color?: string;
+  // room_info?: string;
+  // room_info_stuffix?: string;
+  // room_lights?: string;
+  // show_warning?: boolean;
+  // show_error?: boolean;
+  // test_gui?: boolean;
+  tap_action?: ActionConfig;
+  hold_action?: ActionConfig;
+  double_tap_action?: ActionConfig;
+}
---
+++ src/cards/tcc-camera/tcc-camera.ts
@@ -0,0 +1,102 @@
+import { LitElement, html, TemplateResult, css, PropertyValues, CSSResultGroup } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { registerCustomCard } from "../../helpers";
+import type { TCCCameraCardConfig } from './tcc-camera-config';
+import {
+  HomeAssistant,
+  hasConfigOrEntityChanged,
+  hasAction,
+  ActionHandlerEvent,
+  handleAction,
+  LovelaceCardEditor,
+  getLovelace,
+} from 'custom-card-helpers';
+
+registerCustomCard({
+  type: 'tcc-camera',
+  name: "TCC Camera Card",
+  description: "Camera card",
+});
+
+@customElement('tcc-camera')
+export class TCCCameraCard extends LitElement {
+  // public static async getConfigElement(): Promise<LovelaceCardEditor> {
+  //   await import('./tcc-weather-editor');
+  //   // return document.createElement('tcc-room-card-editor');
+  //   // return document.createElement(LIGHT_CARD_EDITOR_NAME) as LovelaceCardEditor;
+  //   return document.createElement('tcc-room-card-editor') as LovelaceCardEditor;
+  // }
+  
+
+  public static getStubConfig(): Record<string, unknown> {
+    return {};
+  }
+
+  // TODO Add any properities that should cause your element to re-render here
+  // https://lit.dev/docs/components/properties/
+  @property({ attribute: false }) public hass!: HomeAssistant;
+
+  @state() private config!: TCCCameraCardConfig;
+
+  // https://lit.dev/docs/components/properties/#accessors-custom
+  public setConfig(config: TCCCameraCardConfig): void {
+    // TODO Check for required fields and that they are of the proper format
+    if (!config) {
+      throw new Error('You need to define an entity')
+      // throw new Error(localize('common.invalid_configuration'));
+    }
+
+    if (config.test_gui) {
+      getLovelace().setEditMode(true);
+    }
+
+    this.config = {
+      // name: 'Room Name',
+      // room_icon: 'mdi:home-outline',
+      // room_info: 'notset',
+      // room_info_stuffix: '°C',
+      ...config,
+    };
+  }
+
+  // https://lit.dev/docs/components/lifecycle/#reactive-update-cycle-performing
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    if (!this.config) {
+      return false;
+    }
+
+    return hasConfigOrEntityChanged(this, changedProps, false);
+  }
+
+  protected updated(changedProperties: PropertyValues) {
+    super.updated(changedProperties);
+    if (this.hass && changedProperties.has("hass")) {
+        // this.updateControls();
+        // this.updateBrightness();
+    }
+  }
+
+  // Computed card data
+  private cdata = {
+    bgColor: "var(--card-background-color)",
+  };
+
+  // https://lit.dev/docs/components/rendering/
+  protected render(): TemplateResult | void {
+
+    return html`
+      <ha-card class="tccWeather" style="background-color: ${this.cdata.bgColor};">
+        <p>Camera Card</p>
+      </ha-card>
+    `;
+  }
+
+  // https://lit.dev/docs/components/styles/
+  static get styles(): CSSResultGroup {
+    return css`
+      .tccWeather{
+        padding:3px 12px;
+      }
+    `    
+  }
+}
